67d8cfecc16e14698958ebe7d264d7abdc9b068a
Cochenille <Cochenille@users.noreply.github.com>
2026-09-09 19:56:40 +0000

    deploy: 81463682ef2e43a3eb4ca9aa307e896a475a7395

---
+++ assets/chunks/swimlanesDiagram-VR7AAH4N.CGmJ2Qxv.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.bkHhSslk.js";import{_ as a}from"../app.WpTB-GeA.js";import"./chunk-5VM5RSS4.GkpH5duE.js";import"./chunk-XXDRQBXY.m6uhnTFa.js";import"./chunk-POPQ4Y6H.DS8W9KzU.js";import"./chunk-F27PBJKO.DkzydDsN.js";import"./channel.BHG1Cl_B.js";import"./framework.BL7OzMv1.js";import"./theme.C_gU6Osc.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
